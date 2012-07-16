f0ad1764cd7a72a8535b4b3d496fffcd5f288be0
Chris Sainty <csainty@hotmail.com>
2012-07-16 15:39:52 +1000

    Added an ISessionStore interface that can handle saving and loading a session.
    It works at a low level with the NancyContext and Request so that it can be used both for client and server side storage.

---
+++ src/Nancy/Session/ISessionStore.cs
@@ -0,0 +1,11 @@
+﻿namespace Nancy.Session
+{
+    using System.Collections.Generic;
+
+    public interface ISessionStore
+    {
+        bool TryLoadSession(Request request, out IDictionary<string, object> items);
+        
+        void SaveSession(NancyContext request, IDictionary<string, object> items);
+    }
+}
