df913820f451ac9d16d10ed34ff5ea365fc6e4dc
Chris Sainty <csainty@hotmail.com>
2012-10-04 14:55:24 +0200

    Added a demo session project for quick end to end testing and getting a better feel of how it works from sonsumer side. Is it super duper happy?

---
+++ src/Nancy.Demo.Sessions/SessionBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Nancy.Demo.Sessions
+{
+    using Nancy.Bootstrapper;
+    using Nancy.Session;
+    using TinyIoC;
+
+    public class SessionBootstrapper : DefaultNancyBootstrapper
+    { 
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            InternalConfiguration.SessionStore = typeof(InProcessSessionStore);
+
+            base.ConfigureApplicationContainer(container);
+        }
+    }
+}
---
+++ src/Nancy.Demo.Sessions/SessionsModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nancy.Demo.Sessions
+{
+    public class SessionsModule : NancyModule
+    {
+        public SessionsModule()
+        {
+            Get["/"] = p =>
+            {
+                string data = Session.Foo.HasValue ? Session.Foo : "{no value}";
+                return View["Index", data];
+            };
+            Post["/"] = p =>
+            {
+                Session.Foo = Request.Form.Foo;
+                return Response.AsRedirect("/");
+            };
+        }
+    }
+}
